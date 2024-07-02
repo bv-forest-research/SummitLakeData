f8c570ddf86c4bce2ac3a5af8b27d8203d173b0b
aclason <ajclason@gmail.com>
2024-07-02 14:54:43 -0700

    cwd updates

--- R/CWD_VolumeFunctions.R
+++ R/CWD_VolumeFunctions.R
@@ -12,36 +12,26 @@ CWD_vol_calc <- function(dat_loc, incl_sp_decay = FALSE){
 
   #calculate volume for all years
   #2020 & 2021 considered the same year
-  sl_cwd_21 <- CWD_2021_Vol_calc(CWD_dat = paste0(dat_loc,"EP1162CWDsurvey2020-2021.csv"),
-                                 out_carbon_comp = incl_sp_decay)
-
-
   if(incl_sp_decay == FALSE){
-    cd_cwd_allyears <- rbind(dc_cwd_92[,.(Year,Yrs_Post = 0,Unit, VolumeHa)],
-                             dc_cwd_93[,.(Year,Yrs_Post = 1,Unit = Stand, VolumeHa)],
-                             dc_cwd_11[,.(Year = 2011, Yrs_Post = 19,Unit, VolumeHa)],
-                             dc_cwd_19[,.(Year = 2019, Yrs_Post = 27,Unit, VolumeHa)])
+
+    sl_cwd_21 <- CWD_2021_Vol_calc(out_carbon_comp = FALSE)
 
   }else{
-    cd_cwd_allyears <- rbind(dc_cwd_92[,.(Year,Yrs_Post = 0,Unit, Sp, Decay, VolumeHa)],
-                             dc_cwd_93[,.(Year,Yrs_Post = 1,Unit = Stand, Sp, Decay, VolumeHa)],
-                             dc_cwd_11[,.(Year = 2011, Yrs_Post = 19,Unit, Sp, Decay, VolumeHa)],
-                             dc_cwd_19[,.(Year = 2019, Yrs_Post = 27,Unit, Sp, Decay = Decay_2019, VolumeHa)])
+
+    sl_cwd_21 <- CWD_2021_Vol_calc(out_carbon_comp = TRUE)
+
   }
 
   #clean the species columns
-  cd_cwd_allyears[, Sp := ifelse(Sp=="u","U",
-                                 ifelse(Sp == "", "U",
-                                        ifelse(Sp == "ep", "Ep",
-                                               ifelse(Sp == "Act","Ac",Sp))))]
+  sl_cwd_21[, Sp := ifelse(Species =="Unk","U",Species)]
 
 
-  return(cd_cwd_allyears)
+  return(sl_cwd_21)
 
 }
 
 
-#' 1992 CWD volume calculation
+#' 2020/2021 CWD volume calculation
 #'
 #' @param CWD_dat
 #'
@@ -51,35 +41,57 @@ CWD_vol_calc <- function(dat_loc, incl_sp_decay = FALSE){
 #'  CWD volume (m3/ha) = pi^2/8L  *  sum[D2/cos (A)]
 #'   Where: 	L = length of total transect (horizontal distance (HD) in m)
 #'                                         HD = SD / Square root of [1 + (% slope / 100)2]
-#' slope was not measured so assume total transect length to be 90m
+#' slope was measured - so need to update this equation
 #' D = diameter of each piece of CWD (cm)
 #' A = tilt angle from horizontal for each piece (degrees)
 #' Because tilt angle was not measured in 1992 and 1993, it was assumed to be zero,
 #' so that cos (A) = 1 for all pieces in those years.
 #'
 #' @examples
-CWD_1992_Vol_calc <- function(CWD_dat, out_carbon_comp = FALSE){
+CWD_2021_Vol_calc <- function(CWD_dat = "./data-raw/EP1162CWDsurvey2020-2021.csv",
+                              Horiz_dat = "./data-raw/EP1162CWDsurveyTransectLines2020-2021.csv",
+                              out_carbon_comp = FALSE){
   #-----------------------Prepare data -----------------------------------------#
   # Import 1992
-  CWD.1992<- fread(CWD_dat)
+  CWD.2021 <- data.table::fread(CWD_dat, na.strings = "-")
+  CWD.2021[, `Accum/Odd-length(cm)`:=NULL]
 
+  CWD.2021[, unit := as.numeric(stringr::str_split_fixed(Plot, "-",
+                                                         n=2)[,2])]
+  CWD.2021 <- merge(CWD.2021, SummitLakeData::Treatments, by = c("unit"))
   # Square diameter
-  CWD.1992[, D2_cosA:= Diam_cm^2]
+  CWD.2021[, D2_cosA:= `diam(cm)`^2]
 
-  # Convert individual piece to plot summary for L (length of total transect horizontal distance)
   if(out_carbon_comp == FALSE){
-    CWD.1992_plot <- CWD.1992[, .(D2cosA = sum(D2_cosA)),
-                              by =c("Year", "Unit", "Block", "Treatment", "Unique_plot")]
+    CWD2021_plot <- CWD.2021[, .(D2cosA = sum(na.omit(D2_cosA))),
+                             by =c("Plot", "unit","treatment")]
   }else{
-    #if volume will be used for carbon, need to keep species and decay class columns
-    CWD.1992_plot <- CWD.1992[, .(D2cosA = sum(D2_cosA)),
-                              by =c("Year", "Unit","Sp", "Decay","Block", "Treatment", "Unique_plot")]
+    CWD2021_plot <- CWD.2021[, .(D2cosA = sum(na.omit(D2_cosA))),
+                             by =c("Plot","unit", "treatment",
+                                   "Species", "DecayClass")]
   }
 
-  # Volume (m3/ha) calculation
-  CWD.1992_plot[, VolumeHa:= (pi^2/(8*90)) * D2cosA]
 
-  return(CWD.1992_plot)
+  # Import horizontal transect csv file then convert individual lines to plot summary
+  transect <- data.table::fread(Horiz_dat)
+  transect[, unit := as.numeric(stringr::str_split_fixed(Plot, "-",
+                                                         n=2)[,2])]
+  transect <- merge(transect, SummitLakeData::Treatments, by = c("unit"))
+  transectPlot <- transect[, .(HorDist = sum(HorizontalDist)), by =c("unit","treatment")]
+
+  # Merge horizontal distances with all CWD at the plot level
+  CWD2021_plot <- merge(CWD2021_plot, transectPlot, by = c("unit", "treatment"))
+
+  # Volume (m3/ha) calculation
+  CWD2021_plot[, VolumeHa:= pi^2/(8*HorDist) * D2cosA]
+  CWD2021_plot <- CWD2021_plot[!is.na(Species)]
+
+  CWD2021_plot[, `:=`(Year = 2020, Yrs_Post = 2020-1992, Unit = unit,
+                      Sp = ifelse(Species =="Unk","U",Species),
+                      Decay = DecayClass, VolumeHa = VolumeHa)]
+  CWD2021_plot <- CWD2021_plot[,.(Year, Yrs_Post, Unit, Sp, Decay, VolumeHa)]
+
+  return(CWD2021_plot)
 
 }
 
